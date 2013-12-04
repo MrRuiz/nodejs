e43f6aa3ec00d84e41ec346c70c949cb1ef15031
Rafael Ruiz Morales <rafael.ruiz-morales@acknowledgement.co.uk>
2013-12-04 11:14:24 +0000

    Added jQuery to the project and doing some modifications to the app by using jquery and eiditing the css using the .styl file instead the .css file.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 
 /*
  * GET home page.
+ *
+ * Lista de variables que pueden usarse en la pagina index mediante por ejemplo <%= title %>
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'titulo',
+    pageName: 'ChatRaf',
+  });
 };
